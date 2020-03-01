662676ad3d672a4fedb88e26d0c8cf879f8dbec0
torbjkri <torbjorn.kringeland@gmail.com>
2020-03-01 17:06:35 +0100

    Initial commit with ongoing projects

---
+++ dxball/SpriteRenderer.hpp
@@ -0,0 +1,20 @@
+
+#include <glm/glm.hpp>
+#include <Shader.hpp>
+
+class SpriteRenderer
+{
+public:
+    SpriteRenderer(Shader &shader);
+    ~SpriteRenderer();
+
+    void DrawSpirte(Texture2D &texture, glm::vec2 position,
+                    glm::vec2 size -  glm::vec2(10,10),
+                    GLfloat rotate = 0.0f,
+                    glm::vec3 color = glm::vec3(1.0f));
+private:
+    Shader shader;
+    GLuint quadVAO;
+
+    void initRenderData();
+};
